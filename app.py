f4c11633ca6bcc658a3d19b281d440fe0978bd9b
GenerativeCloud <xtan@outlook.com>
2024-02-03 22:29:58 -0600

    Featue complete.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import math
+from time import sleep
 from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
@@ -12,8 +13,31 @@ pan_position = __PAN_ORIGIN
 tilt_position = __TILT_ORIGIN
 
 @app.context_processor
-def position_processor():
+def export_global_variables():
     return dict(pan_position=pan_position, tilt_position=tilt_position, __PAN_ORIGIN=__PAN_ORIGIN, __TILT_ORIGIN=__TILT_ORIGIN)
+
+def drive_tilt_servo(degrees):
+    global tilt_position, pan_position
+    if tilt_position + degrees > __TILT_MAX:
+        degrees = __TILT_MAX - tilt_position
+    if tilt_position + degrees < __TILT_ORIGIN:
+       degrees = __TILT_ORIGIN - tilt_position    
+    tilt_position += degrees
+    # TODO: drive the SG90 servo to rotate to tilt_position. No need for translation.
+    sleep(0.01)
+    return degrees
+
+def drive_pan_servo(degrees):
+    global tilt_position, pan_position
+    if pan_position + degrees > __PAN_MAX:
+        degrees = __PAN_MAX - pan_position
+    if pan_position + degrees < __PAN_ORIGIN:
+        degrees = __PAN_ORIGIN - pan_position    
+    pan_position += degrees
+    servo_position = pan_position - __PAN_ORIGIN # MG90S postion 0° is -135° in the 3D model. 
+    # TODO: drive the MG90S servo to rotate to pan_position
+    sleep(0.01)
+    return degrees
 
 @app.route('/')
 def index():
@@ -23,24 +47,12 @@ def index():
 def tilt(radians):
     global tilt_position, pan_position
     degrees = math.degrees(float(radians))
-    if tilt_position + degrees > __TILT_MAX:
-        degrees = __TILT_MAX - tilt_position
-    if tilt_position + degrees < __TILT_ORIGIN:
-       degrees = __TILT_ORIGIN - tilt_position
-    tilt_position += degrees
-    # TODO: drive the SG90 servo to rotate to tilt_position
-
-    return jsonify(actual = math.radians(degrees), pan = math.radians(pan_position), tilt = math.radians(tilt_position)), 200
+    actual = drive_tilt_servo(degrees)
+    return jsonify(actual = math.radians(actual), pan = math.radians(pan_position), tilt = math.radians(tilt_position)), 200
 
 @app.route('/pan/<radians>')
 def pan(radians):
     global tilt_position, pan_position
     degrees = math.degrees(float(radians))
-    if pan_position + degrees > __PAN_MAX:
-        degrees = __PAN_MAX - pan_position
-    if pan_position + degrees < __PAN_ORIGIN:
-        degrees = __PAN_ORIGIN - pan_position
-    pan_position += degrees
-    # TODO: drive the MG90S servo to rotate to pan_position
-
-    return jsonify(actual = math.radians(degrees), pan = math.radians(pan_position), tilt = math.radians(tilt_position)), 200
+    actual = drive_pan_servo(degrees)
+    return jsonify(actual = math.radians(actual), pan = math.radians(pan_position), tilt = math.radians(tilt_position)), 200
